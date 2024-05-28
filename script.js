dc39d42d888d6e167e168a94a47320b8ddd4a371
Nicolas Caretta <nicocaretta_20@hotmail.com>
2024-05-28 18:07:43 -0300

    slider

---
+++ script.js
@@ -0,0 +1,4 @@
+const sliderContainer = documet.getElementById('slider-container');
+const slider = document.getElementById('slider');
+const buttonLeft = document.getElementById('button-left');
+const buttonRight = document.getElementById('button-right');
