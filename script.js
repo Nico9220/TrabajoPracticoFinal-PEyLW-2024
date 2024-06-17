ec1ffa2ae9ac65041391abc84155ea6d5e5b769e
Nicolas Caretta <nicocaretta_20@hotmail.com>
2024-06-17 00:40:56 -0300

    equipo

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ const sliderContainer = documet.getElementById('slider-container');
 const slider = document.getElementById('slider');
 const buttonLeft = document.getElementById('button-left');
 const buttonRight = document.getElementById('button-right');
+
+
+// SERVICIOS
+// document.addEventListener('DOMContentLoaded', () => {
+//     const servicios = document.querySelectorAll('.servicio');
+
+//     servicios.forEach(servicio => {
+//         servicio.addEventListener('click', () => {
+//             const info = servicio.querySelector('p');
+//             if (info.style.display === 'block') {
+//                 info.style.display = 'none';
+//             } else {
+//                 info.style.display = 'block';
+//             }
+//         });
+//     });
+// });
+
+// EQUIPO MEDICO
+
+function togglePerfil(element) {
+    element.classList.toggle("active");
+}
